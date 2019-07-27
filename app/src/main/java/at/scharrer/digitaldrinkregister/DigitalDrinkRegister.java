92d9dedb4ef1446d2b71387f8d8ebf44aa4e747a
Bernhard Scharrer <bscharre@htl-steyr.ac.at>
2019-07-27 16:47:24 +0200

    project setup

---
+++ app/src/main/java/at/scharrer/digitaldrinkregister/DigitalDrinkRegister.java
@@ -0,0 +1,13 @@
+package at.scharrer.digitaldrinkregister;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DigitalDrinkRegister extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
