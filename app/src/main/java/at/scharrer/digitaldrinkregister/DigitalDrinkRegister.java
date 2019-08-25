aeda75fa3de413c09c316f09c6a0706549d7f491
Bernhard Scharrer <bernhard.scharrer00@gmail.com>
2019-08-25 13:25:19 +0200

    example commit message

--- app/src/main/java/at/scharrer/digitaldrinkregister/DigitalDrinkRegister.java
+++ app/src/main/java/at/scharrer/digitaldrinkregister/DigitalDrinkRegister.java
@@ -2,12 +2,17 @@ package at.scharrer.digitaldrinkregister;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class DigitalDrinkRegister extends AppCompatActivity {
+
+    private static final String TAG = "[DigitalDrinkRegister]";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mainmenu);
+
+        Log.d(TAG, "Hello world!");
     }
 }
